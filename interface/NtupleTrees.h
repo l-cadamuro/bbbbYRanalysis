319117d9a2809e13aa0ad649a2e46a23973c3415
Luca Cadamuro <lc.cadamuro@gmail.com>
2018-11-10 05:21:44 +0100

    added gen particle branches in input

--- interface/NtupleTrees.h
+++ interface/NtupleTrees.h
@@ -4,6 +4,8 @@
 #include "rdJetPUPPI.h"
 #include "rdGenJet.h"
 #include "rdParticle.h"
+#include "rdElectronLoose.h"
+#include "rdMuonLoose.h"
 #include "rdEvent.h"
 #include <string>
 #include <memory>
@@ -14,10 +16,12 @@ class NtupleTrees {
         NtupleTrees(std::string filelistname, std::string treename);
         ~NtupleTrees(){}
 
-        rdJetPUPPI* get_JetPUPPI() {return JetPUPPI_.get();}
-        rdGenJet*   get_GenJet()   {return GenJet_.get();}
-        rdParticle* get_Particle() {return Particle_.get();}
-        rdEvent*    get_Event()    {return Event_.get();}
+        rdJetPUPPI*      get_JetPUPPI()      {return JetPUPPI_.get();}
+        rdGenJet*        get_GenJet()        {return GenJet_.get();}
+        rdParticle*      get_Particle()      {return Particle_.get();}
+        rdElectronLoose* get_ElectronLoose() {return ElectronLoose_.get();}
+        rdMuonLoose*     get_MuonLoose()     {return MuonLoose_.get();}
+        rdEvent*         get_Event()         {return Event_.get();}
 
         bool Next();
 
@@ -28,12 +32,16 @@ class NtupleTrees {
         TChain JetPUPPI_chain_;
         TChain GenJet_chain_;
         TChain Particle_chain_;
+        TChain ElectronLoose_chain_;
+        TChain MuonLoose_chain_;
         TChain Event_chain_;
 
-        std::unique_ptr<rdJetPUPPI> JetPUPPI_;
-        std::unique_ptr<rdGenJet>   GenJet_;
-        std::unique_ptr<rdParticle> Particle_;
-        std::unique_ptr<rdEvent>    Event_;
+        std::unique_ptr<rdJetPUPPI>      JetPUPPI_;
+        std::unique_ptr<rdGenJet>        GenJet_;
+        std::unique_ptr<rdParticle>      Particle_;
+        std::unique_ptr<rdElectronLoose> ElectronLoose_;
+        std::unique_ptr<rdMuonLoose>     MuonLoose_;
+        std::unique_ptr<rdEvent>         Event_;
 
         std::string treename_;
 
@@ -43,19 +51,23 @@ bool NtupleTrees::Next()
 {
     // FIXME: need a check of the coherence of the trees ?
     bool next = (
-        JetPUPPI_ ->Next() &&
-        GenJet_   ->Next() &&
-        Particle_ ->Next() &&
-        Event_    ->Next()
+        JetPUPPI_      ->Next() &&
+        GenJet_        ->Next() &&
+        Particle_      ->Next() &&
+        ElectronLoose_ ->Next() &&
+        MuonLoose_     ->Next() &&
+        Event_         ->Next()
     );
     return next;
 }
 
 NtupleTrees::NtupleTrees(std::string filelistname, std::string treename):
-    JetPUPPI_chain_ ("ntuple/JetPUPPI"),
-    GenJet_chain_   ("ntuple/GenJet"),
-    Particle_chain_ ("ntuple/Particle"),
-    Event_chain_    ("ntuple/Event"),
+    JetPUPPI_chain_      ("ntuple/JetPUPPI"),
+    GenJet_chain_        ("ntuple/GenJet"),
+    Particle_chain_      ("ntuple/Particle"),
+    ElectronLoose_chain_ ("ntuple/ElectronLoose"),
+    MuonLoose_chain_     ("ntuple/MuonLoose"),
+    Event_chain_         ("ntuple/Event"),
     treename_ (treename)
 {
 
@@ -75,6 +87,8 @@ NtupleTrees::NtupleTrees(std::string filelistname, std::string treename):
             JetPUPPI_chain_.Add(line.c_str());
             GenJet_chain_.Add(line.c_str());
             Particle_chain_.Add(line.c_str());
+            ElectronLoose_chain_.Add(line.c_str());
+            MuonLoose_chain_.Add(line.c_str());
             Event_chain_.Add(line.c_str());
             ++nfiles;
         }
@@ -86,19 +100,23 @@ NtupleTrees::NtupleTrees(std::string filelistname, std::string treename):
     // calling this code will interfere with the readers
     if (debug)
     {
-        std::cout << "JetPUPPI_chain_ size : " << JetPUPPI_chain_.GetEntries() << std::endl;
-        std::cout << "GenJet_chain_ size : "   << GenJet_chain_.GetEntries()   << std::endl;
-        std::cout << "Particle_chain_ size : " << Particle_chain_.GetEntries() << std::endl;
-        std::cout << "Event_chain_ size : "    << Event_chain_.GetEntries()    << std::endl;
+        std::cout << "JetPUPPI_chain_ size : "      << JetPUPPI_chain_.GetEntries()      << std::endl;
+        std::cout << "GenJet_chain_ size : "        << GenJet_chain_.GetEntries()        << std::endl;
+        std::cout << "Particle_chain_ size : "      << Particle_chain_.GetEntries()      << std::endl;
+        std::cout << "ElectronLoose_chain_ size : " << ElectronLoose_chain_.GetEntries() << std::endl;
+        std::cout << "MuonLoose_chain_ size : "     << MuonLoose_chain_.GetEntries()     << std::endl;
+        std::cout << "Event_chain_ size : "         << Event_chain_.GetEntries()         << std::endl;
     }
     */
 
 
     /////////// --- create the trees
-    JetPUPPI_ = std::unique_ptr<rdJetPUPPI> (new rdJetPUPPI(&JetPUPPI_chain_));
-    GenJet_   = std::unique_ptr<rdGenJet>   (new rdGenJet(&GenJet_chain_));
-    Particle_ = std::unique_ptr<rdParticle> (new rdParticle(&Particle_chain_));
-    Event_    = std::unique_ptr<rdEvent>    (new rdEvent(&Event_chain_));
+    JetPUPPI_      = std::unique_ptr<rdJetPUPPI>      (new rdJetPUPPI(&JetPUPPI_chain_));
+    GenJet_        = std::unique_ptr<rdGenJet>        (new rdGenJet(&GenJet_chain_));
+    Particle_      = std::unique_ptr<rdParticle>      (new rdParticle(&Particle_chain_));
+    ElectronLoose_ = std::unique_ptr<rdElectronLoose> (new rdElectronLoose(&ElectronLoose_chain_));
+    MuonLoose_     = std::unique_ptr<rdMuonLoose>     (new rdMuonLoose(&MuonLoose_chain_));
+    Event_         = std::unique_ptr<rdEvent>         (new rdEvent(&Event_chain_));
 }
 
 #endif
